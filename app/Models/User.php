f450f61020cd7947ffd7ee09e1df445157ecce38
Debjit Biswas <debjit012@gmail.com>
2024-06-01 21:36:06 +0530

    wip filament users  can access

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,10 +11,12 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Filament\Panel;
+use Filament\Models\Contracts\FilamentUser;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
+
     public function canAccessPanel(Panel $panel): bool
     {
         return true;
