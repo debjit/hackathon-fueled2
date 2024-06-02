8fa88a2d2cf3a153c9c1b8fd5a4ac6fd754e9034
Debjit Biswas <debjit012@gmail.com>
2024-06-02 19:45:45 +0530

    added unpaid amount for expenses

--- app/Filament/Vendors/Widgets/ExpenseOverview.php
+++ app/Filament/Vendors/Widgets/ExpenseOverview.php
@@ -14,6 +14,8 @@ class ExpenseOverview extends BaseWidget
         return [
             Stat::make('Total Bill Amount', $user->expenses->sum('amount') ?? 'N/A')
                 ->color('success'),
+            Stat::make('Total unpaid Bill Amount', $user->expenses()->where('is_paid', 0)->sum('amount') / 100 ?? 'N/A')
+                ->color('success'),
             Stat::make('Total Bills Nos: ', $user->expenses->count() ?? 'N/A')
                 ->color('success'),
         ];
